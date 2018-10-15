d523e9b2db01297d459ab7d2473c0fe798fe57f9
guttardo <guttardo@dinnisoft.com>
2018-10-15 19:18:10 -0300

    Update sync_cloud.py

--- sync_cloud.py
+++ sync_cloud.py
@@ -1,10 +1,10 @@
 import mysql.connector
 import datetime
 
-bd_local = mysql.connector.connect(user='root', password='raspberry',host='192.168.0.140',database='sensores')
+bd_local = mysql.connector.connect(user='root', password='raspberry',host='192.168.0.140',database='sensores', buffered=True)
 cursor = bd_local.cursor()
 
-bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.62.39',database='sensores')
+bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.62.39',database='sensores', buffered=True)
 cursor2 = bd_cloud.cursor()
 
 query = ("SELECT COD, leitura, data FROM bh1750 ")
