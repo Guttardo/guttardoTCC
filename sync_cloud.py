5a2d3bb9fb1ae399cc95d9d8e7fc15b5f6e2cf0f
guttardo <guttardo@dinnisoft.com>
2018-10-15 19:20:59 -0300

    Update sync_cloud.py

--- sync_cloud.py
+++ sync_cloud.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import datetime
 
-bd_local = mysql.connector.connect(user='root', password='raspberry',host='192.168.0.140',database='sensores', buffered=True)
+bd_local = mysql.connector.connect(user='root', password='raspberry',host='192.168.0.140',database='sensores')
 cursor = bd_local.cursor()
+cursor1 = bd_local.cursor(buffered = True)
 
-bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.62.39',database='sensores', buffered=True)
+bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.62.39',database='sensores')
 cursor2 = bd_cloud.cursor()
 
 query = ("SELECT COD, leitura, data FROM bh1750 ")
@@ -16,7 +17,7 @@ for (COD, leitura, data) in cursor:
     cursor2.execute(ins)
     bd_cloud.commit()
     delet = "DELETE FROM bh1750 WHERE `COD` = '"+COD+"' "
-    cursor.execute(delet)
+    cursor1.execute(delet)
     bd_local.commit()
 
 cursor.close()
