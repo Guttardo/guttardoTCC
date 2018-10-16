0c733f595ee6e8f4dd084ff70bcf34d472378a0d
guttardo <guttardo@dinnisoft.com>
2018-10-16 04:48:53 -0300

    Update sync_cloud.py

--- sync_cloud.py
+++ sync_cloud.py
@@ -4,7 +4,7 @@ import datetime
 bd_local = mysql.connector.connect(user='root', password='raspberry',host='192.168.0.140',database='sensores')
 cursor = bd_local.cursor()
 
-bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.62.39',database='sensores')
+bd_cloud = mysql.connector.connect(user='root', password='q1w2e3rtghnjmk,.;!',host='35.198.12.124',database='sensores')
 cursor2 = bd_cloud.cursor()
 
 
